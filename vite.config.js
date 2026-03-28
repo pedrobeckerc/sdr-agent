d85c8d1390239b97e14b5a1ebcde96792cfd97be
pedrobeckerc <bcpedrobecker@gmail.com>
2026-03-28 16:53:02 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,18 @@
 
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/utalk': {
+        target: 'https://app-utalk.umbler.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api\/utalk/, '/api'),
+        secure: true,
+      }
+    }
+  }
+})
